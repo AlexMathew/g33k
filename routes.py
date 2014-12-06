3c73581390565d0fa7d82b0be971068f53b37b5e
Alex Mathew <alexmathew003@gmail.com>
2014-12-07 04:01:05 +0530

    Add signup pages

--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, redirect, request, session, escape, url_for
+from flask import Flask, render_template, redirect, request, session, escape, url_for, \
+				  flash
 import pymongo
 
 app = Flask(__name__)
+db = pymongo.Connection("mongodb://localhost").g33k
 
 
 @app.route('/')
@@ -26,7 +28,8 @@ def trainer_signup():
 
 @app.route('/learner_signup')
 def learner_signup():
-	return render_template('learner_signup.html')
+	characters = db.characters.find()
+	return render_template('learner_signup.html', characters=characters)
 
 
 @app.route('/authenticate_trainer')
@@ -37,6 +40,16 @@ def auth_trainer():
 @app.route('/authenticate_learner')
 def auth_learner():
 	return render_template('profile.html')
+
+
+@app.route('/verify_trainersignup')
+def verify_trainersignup():
+	return
+
+
+@app.route('/verify_learnersignup')
+def verify_learnersignup():
+	return
 
 
 if __name__ == '__main__':
