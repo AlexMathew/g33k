3d92f2e6a9f5aa026e4310b7b3b0765b53b3bf25
Alex Mathew <alexmathew003@gmail.com>
2014-12-06 23:49:30 +0530

    Add signup/signin pages

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session, escape, url_for
+import pymongo
 
 app = Flask(__name__)
 
@@ -6,6 +7,36 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 	return render_template('home.html')
+
+
+@app.route('/learner')
+def learner():
+	return render_template('learner.html')
+
+
+@app.route('/trainer')
+def trainer():
+	return render_template('trainer.html')
+
+
+@app.route('/trainer_signup')
+def trainer_signup():
+	return render_template('trainer_signup.html')
+
+
+@app.route('/learner_signup')
+def learner_signup():
+	return render_template('learner_signup.html')
+
+
+@app.route('/authenticate_trainer')
+def auth_trainer():
+	return render_template('profile.html')
+
+
+@app.route('/authenticate_learner')
+def auth_learner():
+	return render_template('profile.html')
 
 
 if __name__ == '__main__':
