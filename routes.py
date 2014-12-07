d77404ae0d0ded59632f541aebd45288c34b427b
Alex Mathew <alexmathew003@gmail.com>
2014-12-07 18:31:33 +0530

    Add view decorator; add handlers for profile views

--- routes.py
+++ routes.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template, redirect, request, session, escape, url_for, \
-				  flash
-import os				  
+from flask import Flask, render_template, redirect, request, session, \
+                  flash, url_for 
+import os                 
 import pymongo
 import requests
+from functools import wraps
 
 app = Flask(__name__)
 app.secret_key = os.environ['SECRET_KEY']
 db = pymongo.Connection("mongodb://localhost").g33k
 
 
-def mailgun(email, type):
+def mailgun(email, name, type):
     key = os.environ["MAILGUN_KEY"]
     with open('data/welcomemail.txt') as f:
-    	text = f.read()
-    	text = text.replace("...type...", type)
+        text = f.read()
+        text = text.replace("...type...", type)
+        text = text.replace("...name...", name)
     r = requests.post(
             "https://api.mailgun.net/v2/sandboxaafd9ee615e54f49af424db82ccf028a.mailgun.org/messages",
             auth=("api", key),
@@ -27,132 +29,162 @@ def mailgun(email, type):
     return
 
 
+def login_required(f):
+    @wraps(f)
+    def function(*args, **kwargs):
+        if 'username' not in session:
+            return redirect(url_for('home'))
+        return f(*args, **kwargs)
+    return function
+    
+
 @app.route('/')
 def home():
-	if 'username' in session:
-		return render_template('index.html')
-	session['type'] = 'unknown'
-	return render_template('home.html')
+    if 'username' in session:
+        return render_template('index.html')
+    session['type'] = 'unknown'
+    return render_template('home.html')
 
 
 @app.route('/learner', methods=['GET', 'POST'])
 def learner():
-	if request.method == 'POST':
-		username = request.form['username']
-		password = request.form['password']
-		user = db.learners.find_one({ '_id': username })
-		if user is not None and user['_id'] == username and user['password'] == hash(password):
-			session['username'] = username
-			session['type'] = 'learner'
-			return redirect('index')
-		else:
-			flash('Invalid username/password')
-			return redirect('learner')			
-	return render_template('learner.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = db.learners.find_one({ '_id': username })
+        if user is not None and user['_id'] == username and user['password'] == hash(password):
+            session['username'] = username
+            session['type'] = 'learner'
+            return redirect('index')
+        else:
+            flash('Invalid username/password')
+            return redirect('learner')          
+    return render_template('learner.html')
 
 
 @app.route('/trainer', methods=['GET', 'POST'])
 def trainer():
-	if request.method == 'POST':
-		username = request.form['username']
-		password = request.form['password']
-		user = db.trainers.find_one({ '_id': username })
-		if user is not None and user['_id'] == username and user['password'] == hash(password):
-			session['username'] = username
-			session['type'] = 'trainer'
-			return redirect('index')
-		else:
-			flash('Invalid username/password')
-			return redirect('trainer')			
-	return render_template('trainer.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = db.trainers.find_one({ '_id': username })
+        if user is not None and user['_id'] == username and user['password'] == hash(password):
+            session['username'] = username
+            session['type'] = 'trainer'
+            return redirect('index')
+        else:
+            flash('Invalid username/password')
+            return redirect('trainer')          
+    return render_template('trainer.html')
 
 
 @app.route('/trainer_signup')
 def trainer_signup():
-	return render_template('trainer_signup.html')
+    return render_template('trainer_signup.html')
 
 
 @app.route('/learner_signup')
 def learner_signup():
-	characters = db.characters.find()
-	return render_template('learner_signup.html', characters=characters)
+    characters = db.characters.find()
+    return render_template('learner_signup.html', characters=characters)
 
 
 @app.route('/verify_trainersignup', methods=['POST'])
 def verify_trainersignup():
-	username = request.form['username']
-	email = request.form['email']
-	password = request.form['password']
-	trainers = db.trainers
-	if trainers.find_one({ '_id': username }) is None:
-		trainers.insert({
-			'_id': username,
-			'email': email,
-			'password': hash(password)
-			})
-		session['username'] = username
-		session['type'] = 'trainer'
-		mailgun(email, 'trainer')
-		return redirect('index')
-	else:
-		flash('The username "%s" is already taken' % (username))
-		return redirect('trainer_signup')
+    username = request.form['username']
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    trainers = db.trainers
+    if trainers.find_one({ '_id': username }) is None:
+        trainers.insert({
+            '_id': username,
+            'name': name,
+            'email': email,
+            'password': hash(password)
+            })
+        session['username'] = username
+        session['type'] = 'trainer'
+        mailgun(email, 'trainer')
+        return redirect('index')
+    else:
+        flash('The username "%s" is already taken' % (username))
+        return redirect('trainer_signup')
 
 
 @app.route('/verify_learnersignup', methods=['POST'])
 def verify_learnersignup():
-	username = request.form['username']
-	email = request.form['email']
-	password = request.form['password']
-	squad = request.form['squad']
-	squadname = request.form['squadname']
-	resignup = False
-	learners = db.learners
-	if learners.find_one({ '_id': username }) is not None:
-		resignup = True
-		flash('The username "%s" is already taken' % (username))
-	members = set([name.strip().title() for name in squad.split(',')])
-	if len(members) is not 5:
-		resignup = True
-		flash('You should have exactly 5 members in your squad')
-	characters = {result['name'] for result in db.characters.find({}, {'_id': 0, 'name': 1})}
-	if len(members - characters) != 0:
-		resignup = True
-		flash('You should have members only from the list given')
-	if resignup:
-		return redirect('learner_signup')
-	else:
-		squadstats = [db.characters.find_one({'name': member}) for member in members]
-		learners.insert({
-			'_id': username,
-			'email': email,
-			'password': hash(password),
-			'squadname': squadname,
-			'squad': [
-				{
-					'name': member['name'],
-					'skills-web': member['skills-web'],
-					'skills-mobile': member['skills-mobile'],
-					'skills-design': member['skills-design'],
-					'skills-databases': member['skills-databases'],
-					'skills-systems': member['skills-systems']
-				}
-				for member in squadstats
-				]
-			})
-		session['username'] = username
-		session['type'] = 'learner'
-		mailgun(email, 'learner')
-		return redirect('index')
+    username = request.form['username']
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    squad = request.form['squad']
+    squadname = request.form['squadname']
+    resignup = False
+    learners = db.learners
+    if learners.find_one({ '_id': username }) is not None:
+        resignup = True
+        flash('The username "%s" is already taken' % (username))
+    members = set([name.strip().title() for name in squad.split(',')])
+    if len(members) is not 5:
+        resignup = True
+        flash('You should have exactly 5 members in your squad')
+    characters = {result['name'] for result in db.characters.find({}, {'_id': 0, 'name': 1})}
+    if len(members - characters) != 0:
+        resignup = True
+        flash('You should have members only from the list given')
+    if resignup:
+        return redirect('learner_signup')
+    else:
+        squadstats = [db.characters.find_one({'name': member}) for member in members]
+        learners.insert({
+            '_id': username,
+            'name': name,
+            'email': email,
+            'password': hash(password),
+            'squadname': squadname,
+            'squad': [
+                {
+                    'name': member['name'],
+                    'skills-web': member['skills-web'],
+                    'skills-mobile': member['skills-mobile'],
+                    'skills-design': member['skills-design'],
+                    'skills-databases': member['skills-databases'],
+                    'skills-systems': member['skills-systems']
+                }
+                for member in squadstats
+                ]
+            })
+        session['username'] = username
+        session['type'] = 'learner'
+        mailgun(email, 'learner')
+        return redirect('index')
 
 
 @app.route('/index')
+@login_required
 def index():
-	return render_template(
-		'index.html',
-		learner=(session['type']=="learner"),
-		trainer=(session['type']=="trainer")
-		)
+    return render_template(
+        'index.html',
+        learner=(session['type']=="learner"),
+        trainer=(session['type']=="trainer")
+        )
+
+
+@app.route('/trainer/<username>')
+def trainerprofile(username):
+    return render_template('trainerprofile.html')
+
+
+@app.route('/learner/<username>')
+def learnerprofile(username):
+    return render_template('learnerprofile.html')
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return redirect(url_for(session['type'] + 'profile', username=session['username']))
 
 
 @app.route('/logout')
@@ -163,4 +195,4 @@ def logout():
 
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
